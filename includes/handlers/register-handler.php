bb5aa793190f6dee8947b8808fe778c42bf1b70d
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2019-09-28 23:09:57 +0200

    register php code done

---
+++ includes/handlers/register-handler.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+
+function sanitizeFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	// strip_tags tna7i les tags html au cas wa7ed darhom fl input bch yb3thom l bdd ta3na
+	return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	//hadi tbdl str louwl b 2eme
+	return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	//str to lower bayna w ucfirst tkbr la lettre louwla
+	return $inputText;
+}
+
+
+
+if(isset($_POST['registerButton'])) {
+	//isset ta3 ki n3bzo 3al button ha reb hah
+	$username = sanitizeFormUsername($_POST['username']);
+	
+	$firstName = sanitizeFormString($_POST['firstName']);
+
+	$lastName = sanitizeFormString($_POST['lastName']);
+
+	$email = sanitizeFormString($_POST['email']);
+
+	$email2 = sanitizeFormString($_POST['email2']);
+
+	$password = sanitizeFormPassword($_POST['password']);
+
+	$password2 = sanitizeFormPassword($_POST['password2']);
+
+}
+
+
+?>
--- register.php
+++ register.php
@@ -1,3 +1,8 @@
+<?php
+	include("includes/handlers/register-handler.php");	
+	include("includes/handlers/login-handler.php");	
+	?>
+
 <html>
 <head>
 	<title>Welcome to HouseMusic!</title>
@@ -6,7 +11,7 @@
 
 	<div id="inputContainer">
 		<form id="loginForm" action="register.php" method="POST">
-			<h2>LOGIN</h2>
+			<h2>Login !</h2>
 			<p>
 				<label for="loginUsername">Username</label>
 				<input id="loginUsername" name="loginUsername" type="text" placeholder="ex. The360GUnnER" required>
@@ -17,6 +22,47 @@
 			</p>
 
 			<button type="submit" name="loginButton">LOG IN</button>
+			
+		</form>
+
+		<form id="registerForm" action="register.php" method="POST">
+			<h2>Create account !</h2>
+			<p>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="ex. The360GUnnER" required>
+			</p>
+
+			<p>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="ex. Houssem" required>
+			</p>
+
+			<p>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="ex. Kentache" required>
+			</p>
+
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="ex. mata7chilich@gmail.com" required>
+			</p>
+
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="ex. mata7chilich@gmail.com" required>
+			</p>
+
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="registerButton">SIGN UP</button>
 			
 		</form>
 	</div>
