d5ce312ece18b25687ff5bd0f40ce377e8815c17
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2019-10-03 13:14:11 +0200

    register form and php done (no sql for now)

--- register.php
+++ register.php
@@ -40,32 +40,32 @@
 			<p>
 			<?php echo $account->getError(Constants::$usernameCharacters); ?>
 				<label for="username">Username</label>
-				<input id="username" name="username" type="text" placeholder="ex. The360GUnnER" required>
+				<input id="username" name="username" type="text" placeholder="ex. The360GUnnER" value="<?php getInputValue('username') ?>" required>
 			</p>
 
 			<p>
 			<?php echo $account->getError(Constants::$firstNameCharacters); ?>
 				<label for="firstName">First name</label>
-				<input id="firstName" name="firstName" type="text" placeholder="ex. Houssem" required>
+				<input id="firstName" name="firstName" type="text" placeholder="ex. Houssem" value="<?php getInputValue('firstName') ?>" required>
 			</p>
 
 			<p>
 			<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 				<label for="lastName">Last name</label>
-				<input id="lastName" name="lastName" type="text" placeholder="ex. Kentache" required>
+				<input id="lastName" name="lastName" type="text" placeholder="ex. Kentache" value="<?php getInputValue('lastName') ?>" required>
 			</p>
 
 			<p>
 			<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
 			<?php echo $account->getError(Constants::$emailInvalid); ?>
 				<label for="email">Email</label>
-				<input id="email" name="email" type="email" placeholder="ex. mata7chilich@gmail.com" required>
+				<input id="email" name="email" type="email" placeholder="ex. mata7chilich@gmail.com" value="<?php getInputValue('email') ?>" required>
 			</p>
 
 			<p>
 
 				<label for="email2">Confirm email</label>
-				<input id="email2" name="email2" type="email" placeholder="ex. mata7chilich@gmail.com" required>
+				<input id="email2" name="email2" type="email" placeholder="ex. mata7chilich@gmail.com" value="<?php getInputValue('email2') ?>" required>
 			</p>
 
 			<p>
