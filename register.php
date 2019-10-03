364f1cb693596444a07a5e67d302761a64de810d
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2019-10-03 13:08:52 +0200

    register form and php done (no sql for now)

--- register.php
+++ register.php
@@ -1,11 +1,17 @@
 <?php
 	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
 
 	$account = new Account();
-	$account->register();
 
 	include("includes/handlers/register-handler.php");
 	include("includes/handlers/login-handler.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
 ?>
 <html>
 <head>
@@ -32,31 +38,40 @@
 		<form id="registerForm" action="register.php" method="POST">
 			<h2>Create account !</h2>
 			<p>
+			<?php echo $account->getError(Constants::$usernameCharacters); ?>
 				<label for="username">Username</label>
 				<input id="username" name="username" type="text" placeholder="ex. The360GUnnER" required>
 			</p>
 
 			<p>
+			<?php echo $account->getError(Constants::$firstNameCharacters); ?>
 				<label for="firstName">First name</label>
 				<input id="firstName" name="firstName" type="text" placeholder="ex. Houssem" required>
 			</p>
 
 			<p>
+			<?php echo $account->getError(Constants::$lastNameCharacters); ?>
 				<label for="lastName">Last name</label>
 				<input id="lastName" name="lastName" type="text" placeholder="ex. Kentache" required>
 			</p>
 
 			<p>
+			<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+			<?php echo $account->getError(Constants::$emailInvalid); ?>
 				<label for="email">Email</label>
 				<input id="email" name="email" type="email" placeholder="ex. mata7chilich@gmail.com" required>
 			</p>
 
 			<p>
+
 				<label for="email2">Confirm email</label>
 				<input id="email2" name="email2" type="email" placeholder="ex. mata7chilich@gmail.com" required>
 			</p>
 
 			<p>
+			<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+			<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+			<?php echo $account->getError(Constants::$passwordCharacters); ?>
 				<label for="password">Password</label>
 				<input id="password" name="password" type="password" placeholder="Your password" required>
 			</p>
